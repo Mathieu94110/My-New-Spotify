528e075858b67a27e112296d315a9e21320dd9c3
Mathieu Enault <me94110@gmail.com>
2021-03-15 19:06:34 +0100

    Updates

---
+++ precache-manifest.cdc9fc409bed892c864eb4404b165911.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33cf28ebcf465e485319c123bea76bc",
+    "url": "/My-New-Spotify/index.html"
+  },
+  {
+    "revision": "9b1b495efa74a8038e42",
+    "url": "/My-New-Spotify/static/css/main.4e43393c.chunk.css"
+  },
+  {
+    "revision": "912d8c5c504dc5b4537a",
+    "url": "/My-New-Spotify/static/js/2.23e06f3f.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/My-New-Spotify/static/js/2.23e06f3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1b495efa74a8038e42",
+    "url": "/My-New-Spotify/static/js/main.47b86ff2.chunk.js"
+  },
+  {
+    "revision": "aa6b7e8e52844b3db262",
+    "url": "/My-New-Spotify/static/js/runtime-main.07d48b0e.js"
+  },
+  {
+    "revision": "c639d0d3b369076f43b9c2a7124052c8",
+    "url": "/My-New-Spotify/static/media/home_page_spotify.c639d0d3.jpg"
+  },
+  {
+    "revision": "7bcb5a3f982e2e77b9c7ea6dd3d95dc8",
+    "url": "/My-New-Spotify/static/media/logo_spotify.7bcb5a3f.png"
+  }
+]);
